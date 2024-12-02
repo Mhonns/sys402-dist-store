81c6d44e3134b923600f196ed15d18c4ec71eff8
Mhonns <nsamaira@cmkl.ac.th>
2024-12-02 19:29:16 +0000

    Add the test grpc

---
+++ build/CMakeFiles/protolib.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/protolib.dir/proto/hearty-store.grpc.pb.cc.o"
+  "CMakeFiles/protolib.dir/proto/hearty-store.grpc.pb.cc.o.d"
+  "CMakeFiles/protolib.dir/proto/hearty-store.pb.cc.o"
+  "CMakeFiles/protolib.dir/proto/hearty-store.pb.cc.o.d"
+  "libprotolib.a"
+  "libprotolib.pdb"
+  "proto/hearty-store.grpc.pb.cc"
+  "proto/hearty-store.grpc.pb.h"
+  "proto/hearty-store.pb.cc"
+  "proto/hearty-store.pb.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/protolib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
